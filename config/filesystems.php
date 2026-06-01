bc89a79abf77cf1e9fbf3aa0ef4e76bb3fb0639a
adeliahk5474 <adeliahk5474@gmail.com>
2026-06-01 20:13:09 +0700

    cuman dikit cok

--- config/filesystems.php
+++ config/filesystems.php
@@ -74,11 +74,12 @@ return [
             'throw' => false,
         ],
 
-    'cloudinary' => [
-        'driver' => 'cloudinary',
-        'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
-        'api_key' => env('CLOUDINARY_API_KEY'),
-        'api_secret' => env('CLOUDINARY_API_SECRET'),
+        'cloudinary' => [
+            'driver' => 'cloudinary',
+            'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+            'api_key' => env('CLOUDINARY_API_KEY'),
+            'api_secret' => env('CLOUDINARY_API_SECRET'),
+        ]
 
     ],
 
